b36fc07c98a3cfb18ec67add777c249fc771a1ff
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-23 22:10:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board
+it "display_board" do
   puts "   |   |   "
   puts "___________"
   puts "   |   |   "
   puts "___________"
   puts "   |   |   "
+end display_board
